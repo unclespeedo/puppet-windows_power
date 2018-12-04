0b0a98721925a0a219d5343b39cc39186c8cedb2
Ryan Speed <rspeed@speedo.ca>
2018-12-04 15:16:55 -0800

    change unless command to hopefully get more consistency

--- manifests/schemes/scheme.pp
+++ manifests/schemes/scheme.pp
@@ -75,7 +75,7 @@ define windows_power::schemes::scheme(
             command   => "& ${windows_power::params::powercfg} /DUPLICATESCHEME ${template_scheme} ${scheme_guid}",
             provider  => powershell,
             logoutput => true,
-            unless    => $scheme_check,
+            unless    => "& ${windows_power::params::powercfg} /q ${scheme_guid}",
           }
           -> exec { "rename scheme to ${scheme_name}":
             command   => "& ${windows_power::params::powercfg} /CHANGENAME ${scheme_guid} ${scheme_name}",
